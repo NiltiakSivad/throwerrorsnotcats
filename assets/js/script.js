eefd6265daaee3fe28ad814d104b90b26ef9f4e8
Kaitlin Davis <you@example.com>
2024-03-18 19:22:40 -0600

    Add enlarged modal of career projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,25 +16,41 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+// career project variables
+const careerProjectItem = document.querySelectorAll("[data-career-project]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
 
+// modal variable
+const modalImg = document.querySelector("[data-modal-image]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+const modalDescription = document.querySelector("[data-modal-description]");
+
+
+// modal toggle function
+const resumeProjectModalFunc = function () {
+  modalContainer.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
 // add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
+for (let i = 0; i < careerProjectItem.length; i++) {
 
-  testimonialsItem[i].addEventListener("click", function () {
-
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-
-    testimonialsModalFunc();
-
+  careerProjectItem[i].addEventListener("click", function () {
+    modalTitle.innerHTML = this.querySelector(".career-project-title").innerHTML;
+    modalText.innerHTML = this.querySelector(".career-project-category").innerHTML;
+    modalImg.innerHTML = this.querySelector(".career-project-image").outerHTML;
+    modalDescription.innerHTML = this.querySelector(".career-project-description").innerHTML;
+    resumeProjectModalFunc();
   });
 
 }
+
+// add click event to modal close button
+modalCloseBtn.addEventListener("click", resumeProjectModalFunc);
+overlay.addEventListener("click", resumeProjectModalFunc);
 
 // custom select variables
 const selectItems = document.querySelectorAll("[data-select-item]");
